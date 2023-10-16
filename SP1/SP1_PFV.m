97d7692cc053a181f46cea08a1230e6772f02027
janburian <jburian@students.zcu.cz>
2023-10-16 17:12:50 +0200

    First commit

---
+++ SP1/SP1_PFV.m
@@ -0,0 +1,95 @@
+clc
+close all
+clear all
+
+%% Mereni vzdalenosti pomoci ultrazvuku
+distances = [300, 400, 500, 600, 700, 800, 900, 1000]; % [mm]
+measured_values_1 = [1.98, 3.03, 4.16, 5.29, 6.42, 7.54, 8.68, 9.76]; % [V]; measured from 300 mm to 1000 mm
+measured_values_2 = [1.98, 3.03, 4.16, 5.29, 6.41, 7.5, 8.63, 9.76]; % [V]; measured from 1000 mm to 300 mm (already sorted)
+
+figure;
+scatter(distances, measured_values_1, '+', "blue")
+hold on
+scatter(distances, measured_values_2, '+', "red")
+xlabel("d [mm]")
+ylabel("U [V]")
+title("Mereni vzdalenosti pomoci ultrazvukoveho snimace")
+
+
+% Prolozeni pomoci primky - Staticka charakteristika
+p_1 = polyfit(distances, measured_values_1, 1);
+x = linspace(300, 1000, 1000); % Adapt n for resolution of graph
+y = p_1(1) * x + p_1(2);
+
+figure;
+scatter(distances, measured_values_1, '+', "blue")
+hold on
+scatter(distances, measured_values_2, '+', "red")
+hold on
+plot(x, y, "black"); 
+xlabel("d [mm]")
+ylabel("U [V]")
+title("Staticka charakteristika")
+
+% Chyba opakovatelnosti
+% p_2 = polyfit(measured_values_1, distances, 1);
+% x = linspace(300, 1000, 1000); % Adapt n for resolution of graph
+% y = p_2(1) * x + p_2(2);
+% 
+% figure;
+% scatter(distances, measured_values_1, '+', "blue")
+% hold on
+% scatter(distances, measured_values_2, '+', "red")
+% hold on
+% plot(x, y, "black"); 
+% xlabel("d [mm]")
+% ylabel("U [V]")
+% title("Chyba opakovatelnosti")
+
+%% Mereni teploty
+data_thermometer = readmatrix("./data/teplomer_data_all.csv"); 
+data_thermometer_cleaned = data_thermometer(:,[3:5]); % without NaN values [..., temperature, ...]
+Ts = 0.1; % perioda vzorkovani
+
+figure
+x = linspace(0, length(data_thermometer_cleaned) * Ts, length(data_thermometer_cleaned));
+plot(x, data_thermometer_cleaned(:, 2), "-")
+hold on 
+plot(x, data_thermometer_cleaned(:, 3), "-")
+xlabel("Cas [s]")
+% ylabel("Teplota [°C]")
+title("Mereni teploty")
+yline(60, '--', 'Odkryti vodni lazne');
+legend("Prubeh merene teploty", "Prubeh napeti")
+
+% Zavislost teploty na napeti
+figure
+[max_temp, max_idx] = max(data_thermometer_cleaned(:, 2)); 
+plot(data_thermometer_cleaned(1:max_idx, 2), data_thermometer_cleaned(1:max_idx, 3))
+xlabel("Teplota [°C]")
+ylabel("U [V]")
+title("Zavislost teploty na napeti")
+
+%% Elektromechanicka soustava modelu pruzne hridele
+data_shaft = readmatrix("./data/hridel_ukol_c.csv"); 
+data_shaft_cleaned = data_shaft(:,[3:5]); % without NaN and 0 values
+Ts = 0.02; % perioda vzorkovani
+
+x = linspace(0, length(data_shaft_cleaned) * Ts, length(data_shaft_cleaned));
+figure
+plot(x, 10 * sin(Ts * x))
+hold on
+plot(x, data_shaft_cleaned(:, 2), "-")
+hold on 
+plot(x, data_shaft_cleaned(:, 3), "-")
+%xlim([180 400])
+xlabel("Cas [s]")
+ylabel("U [V]")
+
+
+
+
+
+
+
+
